da6b178ab8c76ae2058f0b9344bde8024730eaad
danbinnun1 <danbinnun1@gmail.com>
2022-03-20 22:24:02 +0200

    finish login

--- website/src/QrCode.tsx
+++ website/src/QrCode.tsx
@@ -1,10 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import fetch from 'node-fetch';
+import  { Navigate } from 'react-router-dom'
 
 
 
 function Qr(props: any) {
     const [image, updateState] = React.useState<any>();
+    const [b, setB]=React.useState(0);
+    const [finished, setFinished]=useState(false);
     useEffect(() => {
         async function getImage() {
             if (props.username ===  undefined){
@@ -12,15 +15,24 @@ function Qr(props: any) {
                 return;
             }
             let res = await fetch('http://localhost:5019/login?username=' + props.username + '&password=' + props.password);
+            // if ((await res.text())==='abcd'){
+            //     return <Navigate to='/register'></Navigate>
+            // }
+            if (res.headers.get('Content-Type')!=='image/png'){
+                setFinished(true);
+            }
+
             let data = await res.blob();
             updateState(URL.createObjectURL(data as Blob));
         }
         getImage();
-    }, [props, image]);
+    }, [props, image, b]);
     useEffect(() => {
-        setInterval(() => {
-            updateState(image);
-        }, 1000);
+        const interval = setInterval(() => {
+            setB(b);
+        }, 10000);
+        return () => clearInterval(interval);
+
     }, []);
 
     function delay(ms: number) {
@@ -29,7 +41,9 @@ function Qr(props: any) {
 
 
     //const response = await fetch('localhost:5019?id=8');
-
+    if (finished){
+        return <Navigate to='/register'></Navigate>
+    }
 
     return (
         <div className="App">
