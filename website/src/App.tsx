b22e5f919d56a741fda12eaa5339b0844676b45a
danbinnun1 <danbinnun1@gmail.com>
2022-03-20 11:09:18 +0200

    router

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,16 +1,17 @@
 import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import fetch, {RequestInit} from 'node-fetch';
-
+import fetch, { RequestInit } from 'node-fetch';
+import { BrowserRouter, Link, Route, Router, Routes } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 
 function App() {
-  const [image, updateState]=React.useState<any>();
-  useEffect(() =>{
+  const [image, updateState] = React.useState<any>();
+  useEffect(() => {
     async function getImage() {
-      let res=await fetch('http://localhost:5019?id=8');
-      let data=await res.blob();
+      let res = await fetch('http://localhost:5019?id=8');
+      let data = await res.blob();
       updateState(URL.createObjectURL(data as Blob));
     }
     getImage();
@@ -34,10 +35,20 @@ function App() {
 
 
   return (
-    <div className="App">
-      
-      {image===undefined?'': <img src={image} alt="icons"></img>}
-    </div>
+
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<h1>abcd</h1>} />
+        </Routes>
+        <Link to="/signup" className="btn btn-primary">Sign up</Link>
+      </BrowserRouter>
+
+
+      <div className="App">
+
+        {image === undefined ? '' : <img src={image} alt="icons"></img>}
+      </div></div>
   );
 }
 
